78bf618bbd2a0f661995e22f15bff55106b144c4
psarros <eleftherios.psarros@ext.arhs-cube.com>
2023-11-11 22:55:07 +0200

    first commit before placing to separated packages

--- java/src/main/java/codingdojo/Address.java
+++ java/src/main/java/codingdojo/Address.java
@@ -1,37 +1,77 @@
 package codingdojo;
 
+/**
+ * The type Address.
+ */
 public class Address {
     private String street;
     private String city;
     private String postalCode;
 
+    /**
+     * Instantiates a new Address.
+     *
+     * @param street     the street
+     * @param city       the city
+     * @param postalCode the postal code
+     */
     public Address(String street, String city, String postalCode) {
-        this.street = street;
-        this.city = city;
-        this.postalCode = postalCode;
+        setStreet(street);
+        setCity(city);
+        setPostalCode(postalCode);
     }
 
+    /**
+     * Gets street.
+     *
+     * @return the street
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Sets street.
+     *
+     * @param street the street
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * Gets postal code.
+     *
+     * @return the postal code
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * Sets postal code.
+     *
+     * @param postalCode the postal code
+     */
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
 
+    /**
+     * Gets city.
+     *
+     * @return the city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Sets city.
+     *
+     * @param city the city
+     */
     public void setCity(String city) {
         this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
     }
 }
--- java/src/main/java/codingdojo/Customer.java
+++ java/src/main/java/codingdojo/Customer.java
@@ -1,9 +1,14 @@
 package codingdojo;
 
+import codingdojo.domain.CustomerType;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The type Customer.
+ */
 public class Customer {
     private String externalId;
     private String masterExternalId;
@@ -16,80 +21,175 @@ public class Customer {
     private String companyNumber;
     private int bonusPoints;
 
-    public void setExternalId(String externalId) {
-        this.externalId = externalId;
-    }
-
-    public void setMasterExternalId(String masterExternalId) {
-        this.masterExternalId = masterExternalId;
-    }
-
+    /**
+     * Gets master external id.
+     *
+     * @return the master external id
+     */
     public String getMasterExternalId() {
         return masterExternalId;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    /**
+     * Sets master external id.
+     *
+     * @param masterExternalId the master external id
+     */
+    public void setMasterExternalId(String masterExternalId) {
+        this.masterExternalId = masterExternalId;
     }
 
+    /**
+     * Gets address.
+     *
+     * @return the address
+     */
     public Address getAddress() {
         return address;
     }
 
+    /**
+     * Sets address.
+     *
+     * @param address the address
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    /**
+     * Gets internal id.
+     *
+     * @return the internal id
+     */
     public String getInternalId() {
         return internalId;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPreferredStore(String preferredStore) {
-        this.preferredStore = preferredStore;
-    }
-
-    public String getPreferredStore() {
-        return preferredStore;
-    }
-
-    public CustomerType getCustomerType() {
-        return customerType;
-    }
-
-    public List<ShoppingList> getShoppingLists() {
-        return shoppingLists;
-    }
-
-    public void setShoppingLists(List<ShoppingList> shoppingLists) {
-        this.shoppingLists = shoppingLists;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCompanyNumber() {
-        return companyNumber;
-    }
-
-    public void setCompanyNumber(String companyNumber) {
-        this.companyNumber = companyNumber;
-    }
-
-    public String getExternalId() {
-        return externalId;
-    }
-
-    public void setCustomerType(CustomerType customerType) {
-        this.customerType = customerType;
-    }
-
+    /**
+     * Sets internal id.
+     *
+     * @param internalId the internal id
+     */
     public void setInternalId(String internalId) {
         this.internalId = internalId;
     }
 
+    /**
+     * Gets preferred store.
+     *
+     * @return the preferred store
+     */
+    public String getPreferredStore() {
+        return preferredStore;
+    }
+
+    /**
+     * Sets preferred store.
+     *
+     * @param preferredStore the preferred store
+     */
+    public void setPreferredStore(String preferredStore) {
+        this.preferredStore = preferredStore;
+    }
+
+    /**
+     * Gets customer type.
+     *
+     * @return the customer type
+     */
+    public CustomerType getCustomerType() {
+        return customerType;
+    }
+
+    /**
+     * Sets customer type.
+     *
+     * @param customerType the customer type
+     */
+    public void setCustomerType(CustomerType customerType) {
+        this.customerType = customerType;
+    }
+
+    /**
+     * Gets shopping lists.
+     *
+     * @return the shopping lists
+     */
+    public List<ShoppingList> getShoppingLists() {
+        return shoppingLists;
+    }
+
+    /**
+     * Sets shopping lists.
+     *
+     * @param shoppingLists the shopping lists
+     */
+    public void setShoppingLists(List<ShoppingList> shoppingLists) {
+        this.shoppingLists = shoppingLists;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets company number.
+     *
+     * @return the company number
+     */
+    public String getCompanyNumber() {
+        return companyNumber;
+    }
+
+    /**
+     * Sets company number.
+     *
+     * @param companyNumber the company number
+     */
+    public void setCompanyNumber(String companyNumber) {
+        this.companyNumber = companyNumber;
+    }
+
+    /**
+     * Gets external id.
+     *
+     * @return the external id
+     */
+    public String getExternalId() {
+        return externalId;
+    }
+
+    /**
+     * Sets external id.
+     *
+     * @param externalId the external id
+     */
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    /**
+     * Add shopping list.
+     *
+     * @param consumerShoppingList the consumer shopping list
+     */
     public void addShoppingList(ShoppingList consumerShoppingList) {
-        ArrayList<ShoppingList> newList = new ArrayList<ShoppingList>(this.shoppingLists);
+        ArrayList<ShoppingList> newList = new ArrayList<>(this.shoppingLists);
         newList.add(consumerShoppingList);
         this.setShoppingLists(newList);
     }
@@ -110,10 +210,20 @@ public class Customer {
         return Objects.hash(externalId, masterExternalId, companyNumber);
     }
 
+    /**
+     * Gets bonus points.
+     *
+     * @return the bonus points
+     */
     public int getBonusPoints() {
         return bonusPoints;
     }
 
+    /**
+     * Sets bonus points.
+     *
+     * @param bonusPoints the bonus points
+     */
     public void setBonusPoints(int bonusPoints) {
         this.bonusPoints = bonusPoints;
     }
